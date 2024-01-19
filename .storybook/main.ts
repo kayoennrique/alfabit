f9fe3f491e07ef57df325290062d3be335459d41
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-19 01:14:25 -0300

    first Doc

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,9 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-
 const config: StorybookConfig = {
-  stories: [
-    "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../components/**/*.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -1,17 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react"
+
 import Button, { ButtonProps } from "./Button"
 
 const meta: Meta<ButtonProps> = {
-    title: 'Button',
-    tags: ['autodocs'],
-    component: Button,
-
+  title: 'Molecules/Button',
+  component: Button,
+  argTypes: {}
 }
 
 export default meta
 
 export const Primary: StoryObj<ButtonProps> = {
-    args: {
-        children: 'Botão'
-    }
+  args: {
+    children: 'Action'
+  }
 }
