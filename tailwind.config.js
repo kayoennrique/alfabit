9fe1dc11ad3a99a0579eb633d527bc8bcecb247c
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-19 02:09:35 -0300

    Other Design Tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+function toRgba(cssVariable) {
+  const color = `var(${cssVariable})`
+  return ({ opacityValue }) => `rgba(${color}, ${opacityValue})`
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,10 +11,42 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      mobile: 'var(--screen-mobile)',
+      tablet: 'var(--screen-tablet)',
+      desktop: 'var(--screen-desktop)',
+      tv: 'var(--screen-tv)',
+    },
     extend: {
       colors: {
-        primary: ({ opacityValue }) => `rgba(var(--primary), ${opacityValue})`,
+        primary: toRgba('--primary'),
+        secondary: toRgba('--secondary'),
+        tertiary: toRgba('--tertiary'),
+        quaternary: toRgba('--quaternary'),
+        hover: toRgba('--hover'),
+        click: toRgba('--click'),
+        icon: toRgba('--icon'),
+        outline: toRgba('--outline'),
+        divider: toRgba('--divider'),
+        'bg-light': toRgba('--bg-light'),
+        'bg-dark': toRgba('--bg-dark'),
+        'bg-disabled': toRgba('--bg-disabled'),
+        'text-primary': toRgba('--text-primary'),
+        'text-secondary': toRgba('--text-secondary'),
+        'text-tertiary': toRgba('--text-tertiary'),
+        'text-disabled': toRgba('--text-disabled'),
       },
+      boxShadow: {
+        sm: 'var(--shadow-sm)',
+        md: 'var(--shadow-md)',
+        lg: 'var(--shadow-lg)',
+        focus: 'var(--shadow-focus)',
+        outline: 'var(--shadow-outline)',
+        'button-focus': 'var(--shadow-button-focus)',
+      },
+      blur: {
+        default: 'var(--blur)',
+      }
     },
   },
   plugins: [],
