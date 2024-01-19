37e73fa9f7f4f336ba5bfe48088f2480da228bd3
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-19 01:46:41 -0300

    Working with colors

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,7 @@ export type ButtonProps = {
 const Button = ({ children, className }: ButtonProps) => {
   return <button
     className={`
-      bg-[#2D5BFF] rounded-[8px] px-[32px] py-[12px] text-white
+    bg-primary rounded-[8px] px-[32px] py-[12px] text-white theme-violet
       ${className}
     `}
   >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'var(--primary)',
+      },
     },
   },
   plugins: [],
