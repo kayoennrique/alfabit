c6aa453e178ee7798f13e891fa39a6962d9adc99
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-19 16:45:17 -0300

    Button disabled

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,15 +2,19 @@ export type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className }: ButtonProps) => {
-  return <button
-    className={`
-    bg-primary rounded-md px-6 py-2 text-white
-      ${className}
-    `}
-  >
-    { children }
-  </button>
+const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
+  const generalStyle = 'rounded-md px-6 py-2'
+  const Btn = (classes: string) => {
+    return <button
+      className={`${generalStyle} ${classes} ${className}`}
+      disabled={disabled}
+      {...rest}
+    >
+      { children }
+    </button>
+  }
+
+  return Btn(disabled ? 'bg-bg-disabled text-text-disabled' : 'bg-primary text-white')
 }
 
 export default Button
