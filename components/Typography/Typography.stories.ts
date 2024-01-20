2040e6eec6d2548f044f142d10742e403ec14be9
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 18:47:45 -0300

    Evolving documentation in the Typography component

--- components/Typography/Typography.stories.ts
+++ components/Typography/Typography.stories.ts
@@ -1,3 +1,4 @@
+// Typography.stories.ts
 import { Meta, StoryObj } from "@storybook/react";
 
 import Typography, { type TypographyProps } from "./Typography";
@@ -17,5 +18,41 @@ export default meta;
 export const Primary: StoryObj<TypographyProps> = {
   args: {
     children: "Text",
+  },
+};
+
+export const TextHeadingH1: StoryObj<TypographyProps> = {
+  args: {
+    children: "Text",
+    element: "h1",
+    size: "title1",
+    className: "font-extrabold",
+  },
+};
+
+export const TextHeadingH2: StoryObj<TypographyProps> = {
+  args: {
+    children: "Text",
+    element: "h2",
+    size: "title2",
+    className: "font-extrabold",
+  },
+};
+
+export const TextHeadingH3: StoryObj<TypographyProps> = {
+  args: {
+    children: "Text",
+    element: "h3",
+    size: "title3",
+    className: "font-extrabold",
+  },
+};
+
+export const TextParagraph: StoryObj<TypographyProps> = {
+  args: {
+    children: "Text",
+    element: "p",
+    size: "md",
+    className: "text-primary",
   },
 };
