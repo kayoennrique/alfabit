f24d67e150705bd7f7353e995ec2514886d07d19
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 00:32:21 -0300

    Using CVA in the Typography component

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,46 +1,46 @@
 import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-export type TypographyProps = {
-    variant?: 'primary' | 'secondary' | 'tertiary';
-    size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3'
-    children: React.ReactNode
-    element?: keyof JSX.IntrinsicElements
-} & JSX.IntrinsicElements["p"];
-
-const variantClassMap = {
+const textVariants = cva("text-gray-primary text-sm font-normal", {
+  variants: {
     variant: {
-        primary: "text-gray-primary",
-        secondary: "text-gray-secondary",
-        tertiary: "text-gray-tertiary",
+      primary: "text-gray-primary",
+      secondary: "text-gray-secondary",
+      tertiary: "text-gray-tertiary",
     },
     size: {
-        xs: "text-xs",
-        sm: "text-sm",
-        md: "text-md",
-        xl: "text-xl",
-        title1: "text-txl",
-        title2: "text-tlg",
-        title3: "text-tmd",
+      xs: "text-xs",
+      sm: "text-sm",
+      md: "text-md",
+      xl: "text-xl",
+      title1: "text-txl",
+      title2: "text-tlg",
+      title3: "text-tmd",
     },
-};
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  },
+});
+
+export type TypographyProps = VariantProps<typeof textVariants> & {
+  children: React.ReactNode;
+  element?: keyof JSX.IntrinsicElements;
+} & JSX.IntrinsicElements["p"];
 
 const Typography = ({
-    variant = "primary",
-    size = "md",
-    children,
-    element = "p",
-    ...rest
+  children,
+  element = "p",
+  className,
+  ...rest
 }: TypographyProps) => {
-    const Element = element as any;
-    return (
-        <Element
-            className={`text-gray-primary text-sm font-normal 
-    ${variantClassMap.variant[variant]} ${variantClassMap.size[size]}`}
-            {...rest}
-        >
-            {children}
-        </Element>
-    );
+  const Element = element as any;
+  return (
+    <Element className={`${textVariants(rest)} ${className}`} {...rest}>
+      {children}
+    </Element>
+  );
 };
 
 export default Typography;
