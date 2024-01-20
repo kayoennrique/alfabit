a4a79823cd1304afb971c3777dd71c62e3ea57c9
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 00:19:31 -0300

    Adding styles and documenting the typography component.

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 
 export type TypographyProps = {
-    variant?: string
-    size?: string
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3'
     children: React.ReactNode
     element?: keyof JSX.IntrinsicElements
+} & JSX.IntrinsicElements["p"];
+
+const variantClassMap = {
+    variant: {
+        primary: "text-gray-primary",
+        secondary: "text-gray-secondary",
+        tertiary: "text-gray-tertiary",
+    },
+    size: {
+        xs: "text-xs",
+        sm: "text-sm",
+        md: "text-md",
+        xl: "text-xl",
+        title1: "text-txl",
+        title2: "text-tlg",
+        title3: "text-tmd",
+    },
 };
 
 const Typography = ({
-    variant = '',
-    size = '',
+    variant = "primary",
+    size = "md",
     children,
-    element = "p"
+    element = "p",
+    ...rest
 }: TypographyProps) => {
-    const Element = element;
-    return <Element>{children}</Element>;
+    const Element = element as any;
+    return (
+        <Element
+            className={`text-gray-primary text-sm font-normal 
+    ${variantClassMap.variant[variant]} ${variantClassMap.size[size]}`}
+            {...rest}
+        >
+            {children}
+        </Element>
+    );
 };
 
 export default Typography;
