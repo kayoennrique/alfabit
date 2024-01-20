650da59eb5972ff201cfd2934e4e26433e5b6eac
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 19:26:16 -0300

    Documenting the colors, and changing the Storybook theme

---
+++ .storybook/alfabitTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Alfabit Design System",
+  brandUrl: "https://https://github.com/kayoennrique/alfabit",
+  brandImage: "./logo-alfabit-branco.png",
+  brandTarget: "_self",
+});
