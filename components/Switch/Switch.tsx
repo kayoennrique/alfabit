cbc894f6d7631120a6f135efca5d15c3e42feb68
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 15:59:06 -0300

    Creating Switch component

---
+++ components/Switch/Switch.tsx
@@ -0,0 +1,14 @@
+import { Switch as HeadlessSwitch, type SwitchProps as HeadlessSwithcProps } from "@headlessui/react"
+
+export type SwitchProps = {
+    defaultEnable?: boolean;
+    variant?: 'common' | 'contract';
+    disabled?: boolean;
+    onChange?: (enabled: boolean) => void;
+}
+
+const Switch = ({defaultEnable: enabledByDefault, variant= 'common', disabled, onChange}: SwitchProps) => {
+    return <div></div>
+}
+
+export default Switch;
