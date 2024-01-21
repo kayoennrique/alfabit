b4e07717d30d3975c78a5fc405aa86bbb48f5ab1
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-20 23:00:47 -0300

    Finalizing Modal documentation

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,18 +1,19 @@
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition, DialogProps } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import React, { Fragment } from "react";
 
 export type ModalProps = {
-    children: string;
+    children: React.ReactNode;
     isOpen: boolean;
     title: string;
     onClose: () => void;
-} & React.HTMLAttributes<HTMLDivElement>;
+} & DialogProps<any> &
+    React.HTMLAttributes<HTMLDivElement>;
 
 const Modal = ({ children, isOpen, onClose, title }: ModalProps) => {
     return (
         <Transition appear show={isOpen} as={Fragment}>
-            <Dialog as="div" className="" onClose={onClose}>
+            <Dialog as="div" className="relative z-10" onClose={onClose}>
                 <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
@@ -22,11 +23,11 @@ const Modal = ({ children, isOpen, onClose, title }: ModalProps) => {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                 >
-                    <div className="" />
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
                 </Transition.Child>
 
-                <div className="">
-                    <div className="">
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -36,13 +37,13 @@ const Modal = ({ children, isOpen, onClose, title }: ModalProps) => {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <Dialog.Panel className="">
-                                <div className="">
-                                    <Dialog.Title as="h3" className="">
+                            <Dialog.Panel className="flex flex-col gap-5 w-full max-w-md transform overflow-hidden rounded-lg bg-white p-7 transition-all">
+                                <div className="flex items-center justify-between">
+                                    <Dialog.Title as="h3" className="text-lg font-semibold">
                                         {title}
                                     </Dialog.Title>
                                     <button onClick={onClose}>
-                                        <XMarkIcon className="" />
+                                        <XMarkIcon className="w-5 h-5 text-disabled" />
                                     </button>
                                 </div>
                                 {children}
